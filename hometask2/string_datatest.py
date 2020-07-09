9b6ce9f0c9c2394e54fa678695eddc751cea99ce
esaganchi <evgeniy.s@ati.su>
2020-07-09 11:37:27 +0300

    new_commit

--- hometask2/string_datatest.py
+++ hometask2/string_datatest.py
@@ -3,5 +3,18 @@ import pytest
 # тесты с типом данных string
 
 def test_string_one():
-    secondname = 'Evgeniy'
-    assert secondname[0] == 'E'
+    second_name = 'Evgeniy'
+    assert second_name[0] == 'E'
+
+
+def test_string_two():
+    long_name = 'evgeniysaganchy'
+    assert long_name[7:] == 'saganchy'
+
+
+def test_string_three():
+    big_name = 'evgeniysaganchi' + 'igorevich'
+    assert big_name != 'evgeniysaganciigorevich'
+
+# def test_string_four():
+
