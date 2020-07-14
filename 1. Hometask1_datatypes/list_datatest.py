f79d19e85fcf5832089f266b0aa83e3c3d0f3ec7
esaganchi <evgeniy.s@ati.su>
2020-07-15 00:37:18 +0300

    list_end

--- 1. Hometask1_datatypes/list_datatest.py
+++ 1. Hometask1_datatypes/list_datatest.py
@@ -1,7 +1,36 @@
-def test_дшые_one():
-    list_one = ['l','i','s','t','1']
+import pytest
+
+# Проверка на то, что первый элемент списка == 'Monday'
+
+def test_list_one():
+    weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+    assert weekdays[0] == 'Monday'
+
+# Проверка на то, что второй элемент списка != 'Monday'
+
+def test_list_two():
+    weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+    assert weekdays[1] != 'Monday'
 
 
+# Проверяем значение элемента в списке по его значению
 
-print (test_string_three())
+def test_list_three():
+    mexicans = ['Marco', 'Garsia', 'Gumo', 'Gonsales', 'Migel']
+    assert mexicans.index('Gumo') != 3
 
+# Проверяем, что элемент встречается в списке 1 раз(Проверяем наличие элемента в списке с помощью оператора in)
+
+def test_list_four():
+    mexicans = ['Marco', 'Garsia', 'Gumo', 'Gonsales', 'Migel']
+    #ssert mexicans.count('Gumo') == 1
+    result = 'Gumo' in mexicans
+    assert result == True
+
+# Parametrize (сумма параметров равна ожидаемому результату)
+
+list = [1,2,3,4]
+@pytest.mark.parametrize('an_element_from_the_list', list)
+def test_list_five(an_element_from_the_list):
+    a = sum(list)
+    assert a == 10
--- 1. Hometask1_datatypes/string_datatest.py
+++ 1. Hometask1_datatypes/string_datatest.py
@@ -14,10 +14,11 @@ def test_string_three():
     big_name = 'saganchi_evgeniy' + '_igorevich'
     assert big_name == 'saganchi_evgeniy_igorevich'
 
-@pytest.mark.parametrize("test_input", [(1,1),(2,2),(3,3)])
+@pytest.mark.parametrize("test_input", [(3,3),(6,6),(7,7)])
 def test_string_four(test_input):
     assert test_input[0] - test_input[1] == 0
 
+# Parametrize
 
 def test_string_five():
     hight_string = 'shkola'
