ef5b05d11835ff17c88168549028a1800e8406f4
esaganchi <evgeniy.s@ati.su>
2020-07-20 12:58:21 +0300

    status

--- 1. Hometask1_datatypes/set_datatest.py
+++ 1. Hometask1_datatypes/set_datatest.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 import pytest
 
 
@@ -8,9 +10,9 @@ import pytest
 def test_set_one():
     numbers = [1, 2, 3, 2, 3, 4, 5]
     set_numbers = set(numbers)
-# print(set_numbers)
+    # print(set_numbers)
     set_numbers.add('666')
-# print(set_numbers)
+    # print(set_numbers)
     assert '666' in set_numbers
 
 
@@ -22,13 +24,15 @@ def test_set_one():
 
 """
 
+
 def test_set_two():
-    days = ['Monday','Tuesday', 'Saturday', 'Friday','Monday']
+    days = ['Monday', 'Tuesday', 'Saturday', 'Friday', 'Monday']
     set_days = set(days)
-    #print(set_days)
+    # print(set_days)
     set_days.remove('Friday')
-    #print(set_days)
+    # print(set_days)
     assert 'Friday' not in set_days
+
 
 # Объединяем множества, проверяем, что подмножество входит во множество
 
@@ -36,7 +40,7 @@ def test_set_three():
     set_one = {'Patrick', 'Ksuha'}
     set_two = {'Serega', 'Jeka'}
     all_set = set_one.union(set_two)
-    #print(all_set)
+    # print(all_set)
     assert set_one.issubset(all_set)
 
 
@@ -46,25 +50,12 @@ def test_set_four():
 
     pass
 
+set1 = {'Patrick', 'Ksuha', 'Serega', 'Elchugin'}
+set2 = {'Elchugin', 'Rita', 'Grishina'}
 
-set1 = {1, 2, 3, 4}
-set2 = {3, 6, 5}
+@pytest.mark.parametrize('an_element_in_the_set', set1, set2)
+def inter(an_element_in_the_set):
+    new_set = set1 ^ set2
+    print(new_set)
+    assert 'Elchugin' not in new_set
 
-@pytest.mark.parametrize('news_set_test', set1, set2)
-def test_set_six(news_set_test):
-    last_set = set1.intersection(set2)
-    assert last_set == 3
-
-
-
-
-
-
-# set1 = {'Patrick', 'Ksuha', 'Serega', 'Elchugin'}
-# set2 = {'Elchugin', 'Rita', 'Grishina'}
-#
-# def inter():
-#     new = set1.intersection(set2)
-#     print(new)
-#
-# inter()
