3a467aa1be78991efa9c9f7d1a4b427fd11d6576
esaganchi <evgeniy.s@ati.su>
2020-07-20 09:58:47 +0300

    dasdasd

--- 1. Hometask1_datatypes/list_datatest.py
+++ 1. Hometask1_datatypes/list_datatest.py
@@ -1,15 +1,17 @@
 import pytest
 
+
 # Проверка на то, что первый элемент списка == 'Monday'
 
 def test_list_one():
-    weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     assert weekdays[0] == 'Monday'
+
 
 # Проверка на то, что второй элемент списка != 'Monday'
 
 def test_list_two():
-    weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     assert weekdays[1] != 'Monday'
 
 
@@ -19,17 +21,21 @@ def test_list_three():
     mexicans = ['Marco', 'Garsia', 'Gumo', 'Gonsales', 'Migel']
     assert mexicans.index('Gumo') != 3
 
+
 # Проверяем, что элемент встречается в списке 1 раз(Проверяем наличие элемента в списке с помощью оператора in)
 
 def test_list_four():
     mexicans = ['Marco', 'Garsia', 'Gumo', 'Gonsales', 'Migel']
-    #ssert mexicans.count('Gumo') == 1
+    # ssert mexicans.count('Gumo') == 1
     result = 'Gumo' in mexicans
     assert result == True
 
+
 # Parametrize (сумма параметров равна ожидаемому результату)
 
-list = [1,2,3,4]
+list = [1, 2, 3, 4]
+
+
 @pytest.mark.parametrize('an_element_from_the_list', list)
 def test_list_five(an_element_from_the_list):
     a = sum(list)
@@ -39,7 +45,7 @@ def test_list_five(an_element_from_the_list):
 def test_set_one():
     numbers = [1, 2, 3, 2, 3, 4, 5]
     set_numbers = set(numbers)
-# print(set_numbers)
+    # print(set_numbers)
     set_numbers.add('666')
-# print(set_numbers)
+    # print(set_numbers)
     assert '666' in set_numbers
--- 1. Hometask1_datatypes/set_datatest.py
+++ 1. Hometask1_datatypes/set_datatest.py
@@ -1,6 +1,7 @@
 import pytest
 
-# Создаем множество при помощи set. Проверяем, что во множество был добавлен элемент
+# Создаем множество из списка при помощи set.
+# Проверяем, что во множество был добавлен элемент
 # и он есть в этом множестве
 
 def test_set_one():
@@ -11,15 +12,32 @@ def test_set_one():
 # print(set_numbers)
     assert '666' in set_numbers
 
-# Проверка на то
+
+"""
+
+Создаем множество из списка при помощи set.
+Проверяем, что из множества был удален элемент
+и его нет в этом множестве
+
+"""
 
 def test_set_two():
+    days = ['Monday','Tuesday', 'Saturday', 'Friday','Monday']
+    set_days = set(days)
+    #print(set_days)
+    set_days.remove('Friday')
+    #print(set_days)
+    assert 'Friday' not in set_days
+
+# Объединение множеств
+
+def test_set_three():
+    set_one = {'Patrick', 'Ksuha'}
+    set_two = {'Serega', 'Jeka'}
+
+
+def test_set_four():
     
 
-# Проверка на то
 
 
-# Проверка на то
-
-
-# Проверка на то
