11532f5d0a9c4781a616c409426aff7d51b467e6
esaganchi <evgeniy.s@ati.su>
2020-07-29 08:58:38 +0300

    new-commit

---
+++ test_request/test_req_otus.py
@@ -0,0 +1,14 @@
+import requests
+
+
+
+
+
+
+
+http://httpbin.org/
+http://httpbin.org/redirect/{n}
+curl -X GET "http://httpbin.org/redirect/1" -H "accept: text/html"
+Написать простой тест на гет с параметром n - количество редиректов
+
+
